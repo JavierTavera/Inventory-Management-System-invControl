c3f4d07609ee7a9ee24211941cf3d8173cde9942
Javier <javieriti@gmail.com>
2023-12-10 19:15:39 -0300

    Setting postgresql

--- base/models.py
+++ base/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Bodega(models.Model):
+    nombre = models.CharField(max_length=80)
+    desc = models.CharField(max_length=200)
--- invcontrol/settings.py
+++ invcontrol/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'invcontrol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invControl',
+        'USER': 'postgres',
+        'PASSWORD': '1234c',
+        'HOST': 'localhost'
     }
 }
 
